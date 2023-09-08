2b95843c848634c21d1337927ed537a2a6352e45
shahid <shahidalloor27@gmail.com>
2023-09-09 00:20:10 +0530

    123

---
+++ madlib.py
@@ -0,0 +1,11 @@
+adj = input("adjective: ")
+verb1 =input("verb1: ")
+verb2 =input("verb2: ")
+famous_person = input("famous_person: ")
+
+
+
+
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time \
+I love to {verb1} . Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
