66795abc991671f8b20c5651f5a521edce58f4d6
shahid <shahidalloor27@gmail.com>
2023-09-09 00:25:09 +0530

    new

--- madlib.py
+++ madlib.py
@@ -6,6 +6,6 @@ famous_person = input("famous_person: ")
 
 
 
-madlib = f"Computer programming is so {adj}! it makes me so excited all the time \
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time .\
 I love to {verb1} . Stay hydrated and {verb2} like you are {famous_person}!"
 print(madlib)
